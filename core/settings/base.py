8e5688fdc6d6ccd09949476b7c0878a3440e4707
Jaime Alberto Suarez Moctezuma <Suarezjaime2712@gmail.com>
2025-01-24 16:22:42 -0600

    Creación de la app api, tambieén se modifico la configuración en una carpeta aparte y la base de datos se paso a MySQL.

--- core/settings/base.py
+++ core/settings/base.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -69,18 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
@@ -105,7 +95,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Mexico_City'
 
 USE_I18N = True
 
---
+++ core/settings/local.py
@@ -0,0 +1,13 @@
+from .base import *
+
+# Database
+# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "OPTIONS": {
+            "read_default_file": "C:\Users\jaime\OneDrive\Escritorio\demo\core\settings\my.cnf",
+        },
+    }
+}
